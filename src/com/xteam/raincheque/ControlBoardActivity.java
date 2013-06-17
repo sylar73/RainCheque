68d9232df7e5b89186b4084c24b3f301def69001
Girish Kamath <girishk91@gmail.com>
2013-06-18 00:37:05 +0530

    Initial commit
    Starting development on RainCheque app

---
+++ src/com/xteam/raincheque/ControlBoardActivity.java
@@ -0,0 +1,25 @@
+package com.xteam.raincheque;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class ControlBoardActivity extends Activity 
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) 
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.control_board);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) 
+	{
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.control_board, menu);
+		return true;
+	}
+
+}
