033b923a30f161719d08c97f1f87762ea262da62
Ajinkya Joshi <ajinkya.josh@gmail.com>
2013-06-18 05:41:58 +0530

    Added AccountRecord class

---
+++ src/com/xteam/raincheque/AccountRecord.java
@@ -0,0 +1,13 @@
+package com.xteam.raincheque;
+
+import java.io.Serializable;
+
+public class AccountRecord implements Serializable
+{
+	private static final long serialVersionUID = -8693621364691235101L;
+	String name;
+	int paid;
+	int worth;
+	int settlement;
+
+}
